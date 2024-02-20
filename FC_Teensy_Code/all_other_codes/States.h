591ac4db812ff38bc4fb5cd476fc08f07fb7c64a
cygnus06 <131586556+cygnus06@users.noreply.github.com>
2024-02-20 20:10:34 +0530

    Update States.h

--- FC_Teensy_Code/all_other_codes/States.h
+++ FC_Teensy_Code/all_other_codes/States.h
@@ -8,7 +8,7 @@ extern bool TouchDown_Detect;
 void Apogee_check(float alt,float vel,float acc, float time);
 void Burnout_detect_fun(float alt, float vel, float acc, float time);
 void detect_LiftOff(float alt, float vel, float time);
-void main_deploy(float alt, float vel);
+void Check_main_deploy(float alt, float vel);
 void check_touchdown();
 //detect touch_down
 
